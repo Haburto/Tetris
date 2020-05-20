c2da46fbf18f818a785efbd102600fbe5389d94b
Haburto <shadowibsack@live.de>
2020-05-20 20:19:13 +0200

    Initial commit with TODO's and comments

---
+++ main.py
@@ -0,0 +1,30 @@
+import pygame
+
+# Tetris - game logic
+# I want to have a level system, each level the gamespeed increases a certain amount which affects the total score
+# How many blocks can fit in the x axis? probably 6 to 8 ?
+
+# TODO: read up on abstract classes and inheritance in python
+# BLOCKS available: 7
+# square, L, backward L, straight line, small T, stair, backward star
+
+# SCORE (from wiki)
+# one line rewards 100 pts, four lines rewards 800 pts (called tetris)
+# each subsequent back-to-back tetris awards 1200 pts
+# each level has a different score-multiplier
+
+# UI
+# HOTKEYS
+# arrows keys: left / right to move that way, up to turn the block, and down for a hard-drop
+# p: to pause
+# r: to reset
+
+# TODO: implement main window
+# TODO: implement grid system
+# TODO: implement UI
+# TODO: pre implement all classes
+# TODO: class Block
+# TODO: class FOR EACH PIECE - see game logic comment
+# TODO: implement gameplay loop - see game logic comment
+# TODO: implement hotkey support
+# TODO: implement logic
