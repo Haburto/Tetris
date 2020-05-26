99b9250672b9138eb06ddfd8778b394f9938685f
Haburto <shadowibsack@live.de>
2020-05-27 00:39:22 +0200

    implemented: surface_handler(), draw_game_area(), added new TODO's

--- main.py
+++ main.py
@@ -31,14 +31,51 @@ import pygame
 # TODO: implement gameplay loop - see game logic comment
 # TODO: implement hotkey support
 # TODO: implement logic
-window_size = (800, 600)
-window_fill_colour = (0, 0, 0)
+# TODO: add a title
+# TODO: add a logo
+# TODO: add background music
+# TODO: add sound effects for ingame events
+# TODO: (maybe) add a background image
+window_size = (1200, 800)
+game_area_size = (window_size[0] * 1 // 2, window_size[1] * 4 // 5)  # 2/3 of the width, 4/5 of the height
+
+window_fill_colour = (10, 50, 10)
+game_area_color = (150, 150, 150)
+
 window = pygame.display.set_mode(window_size)
 
 quit_game = False
 
 
+def draw_game_area():
+    """Draws the game area."""
+    game_area_x_start = (window_size[0] - game_area_size[0]) // 2
+    game_area_y_start = (window_size[1] - game_area_size[1]) // 2
+    game_area_xy_start = (game_area_x_start, game_area_y_start)
+
+    pygame.draw.rect(window,
+                     game_area_color,
+                     (game_area_xy_start[0], game_area_xy_start[1], game_area_size[0], game_area_size[1])
+                     )
+
+
+def test_method():
+    """This method only exists to test new implementations - delete it later!"""
+    pass
+
+
+def surface_handler():
+    """Calls all other surface affecting methods."""
+    window.fill(window_fill_colour)
+    draw_game_area()
+    # TEST AREA START - # TODO: refactor and delete this part later! Do not forget!
+    test_method()
+    # TEST AREA END
+    pygame.display.update()
+
+
 def event_handler():
+    """Handles game events like keyboard and mouse input."""
     global quit_game
 
     for event in pygame.event.get():
@@ -49,9 +86,7 @@ def event_handler():
 def main():
     while not quit_game:
         event_handler()
-
-        window.fill(window_fill_colour)
-        pygame.display.update()
+        surface_handler()
 
     pygame.quit()
     exit()
